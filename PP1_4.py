052199934d75c7e39e51ec9dedcc022d5af50b8c
Gajaanan-Varatharasan-ICD201-01 <gajaanan.varatharasan@student.tdsb.on.ca>
2024-09-19 13:40:03 +0000

    gd

--- PP1_4.py
+++ PP1_4.py
@@ -2,16 +2,22 @@
 
 def q1():
   #Write Assignment code here
-
+  word=input("Enter a word: ")
+  print(word)
 def q2():
   #Write Assignment code here
-
+  name=input("Enter your first name")
+  print(f"Hello {name}")
 def q3():
   #Write Assignment code here
-
+  fname=input("Enter first name")
+  lname=input("Enter last name")
+  print(f"Hello {lname} {fname}")
 def q4():
   #Write Assignment code here
-
+  student1=input("Enter name of first student: ")
+  student2=input("Enter name of second student: ")
+  print(f"Your students are {student1} and {student2}")
 #Do not edit code below this comment
 
 q1()
