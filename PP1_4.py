704af0ca542389910f097a2d9ae44e002bb1b845
Gajaanan-Varatharasan-ICD201-01 <gajaanan.varatharasan@student.tdsb.on.ca>
2024-11-04 11:22:45 -0500

    Update PP1_4.py

--- PP1_4.py
+++ PP1_4.py
@@ -19,3 +19,8 @@ def q4():
   student2=input("Enter name of second student: ")
   print(f"Your students are {student1} and {student2}")
 #Do not edit code below this comment
+
+#q1():
+#q2():
+#q3():
+#q4():
